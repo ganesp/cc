bc7721cfbefe5874e8783f705a6694867d8a134b
Siddhartha Pandey <Siddhartha.Pandey@microsoft.com>
2019-03-29 08:00:41 +0000

    fixed in progress case

--- CCPatch/patch.ps1
+++ CCPatch/patch.ps1
@@ -13,23 +13,15 @@ $authHeader = ConvertTo-BasicAuthHeader $env:SYSTEM_ACCESSTOKEN
 
 $projectId = $env:SYSTEM_TEAMPROJECTID
 
-$uri = $env:TCMADDRESS + '/' + $projectId + '/_apis/testresults/CodeCoverage/?buildId=' + $env:BUILD_BUILDID + '&api-version=5.0-preview.1'
+# Sample links
+# https://vstmr.codedev.ms/foo/0e804a94-5253-4fb8-a183-7c79420142ff/_apis/testresults/CodeCoverage/?buildId=1234&api-version=5.0-preview.1
+# https://tcm1.dev.azure.com/foo/0e804a94-5253-4fb8-a183-7c79420142ff/_apis/testresults/CodeCoverage/?buildId=1234&api-version=5.0-preview.1
+
+$uri = 'https://vstmr.codedev.ms/CodeCoverageDemo' + '/' + $projectId + '/_apis/testresults/CodeCoverage/?buildId=' + $env:BUILD_BUILDID + '&api-version=5.0-preview.1'
 
 Write-Host 'Calling patch via ' $uri
 
-$patchRequest = Invoke-WebRequest -Uri $uri -Headers $authHeader -Method Patch -ContentType 'application/json' -UseBasicParsing
-
-try
-{
-    Invoke-WebRequest -Uri $uri -Headers $authHeader -Method Patch -ContentType 'application/json' -UseBasicParsing
-}
-catch
-{
-    if($_.Exception.Response.StatusCode -ne 425) {
-        Write-Host 'Conflict response didnt match, ' + $response
-        exit 1;
-    }
-}
+$patchRequest = Invoke-WebRequest -Uri $uri -Headers $authHeader -Method Patch -ContentType 'application/json'
 
 $patchResult = $patchRequest.Content | ConvertFrom-Json
 
@@ -40,14 +32,21 @@ if($patchResult.status -ne 'pending') {
 
 $getRequest = ''
 
+# Expected Status Values
+# 'pending' The coverage merge/evaluation is queued and hasnt started yet
+# 'inProgress' The coverage merge/evaluation is in progress
+# 'completed' The coverage merge/evaluation for the request client made is completed
 do
 {
     Start-Sleep -s 1
-    $getRequest = Invoke-RestMethod -Uri $uri -Headers $authHeader -Method Get -ContentType 'application/json' -UseBasicParsing
-} while ($getRequest.status -eq 'pending')
+    $getRequest = Invoke-RestMethod -Uri $uri -Headers $authHeader -Method Get -ContentType 'application/json'
+} while ($getRequest.status -eq 'pending' -or  $getRequest.status -eq 'inProgress')
 
 
 if($getRequest.status -ne 'completed') {
     Write-Host 'status is not completed, ' + $getRequest.status
     exit 1;
 }
+
+Write-Host $getRequest.status
+Write-Host $getRequest.coverageData.coverageStats
