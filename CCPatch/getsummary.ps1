6ac070b4982314f44e184799b20243177bf12632
Kavipriya Adhinarayanan <kaadhina@microsoft.com>
2019-05-27 13:41:19 +0530

    Adding timeout in script

--- CCPatch/getsummary.ps1
+++ CCPatch/getsummary.ps1
@@ -27,11 +27,12 @@ $getRequest = ''
 # 'pending' The coverage merge/evaluation is queued and hasnt started yet
 # 'inProgress' The coverage merge/evaluation is in progress
 # 'completed' The coverage merge/evaluation for the request client made is completed
+$startTime = Get-Date
 do
 {
     Start-Sleep -s 1
     $getRequest = Invoke-RestMethod -Uri $uri -Headers $authHeader -Method Get -ContentType 'application/json' -UseBasicParsing
-} while ($getRequest.status -eq 'pending' -or  $getRequest.status -eq 'inProgress')
+} while (($getRequest.status -eq 'pending' -or  $getRequest.status -eq 'inProgress') -and ((Get-Date) -lt $startTime.AddMinutes(3)))
 
 if($getRequest.status -ne 'completed') {
     Write-Host 'status is not completed, ' + $getRequest.status
